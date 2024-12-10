5174cd82c7b95a732fcbab1da082b4e043b6ef10
RetnoNS <119503025+RetnoNS@users.noreply.github.com>
2024-12-10 16:00:32 +0700

    upload

--- afterlogin/afterlogin.js
+++ afterlogin/afterlogin.js
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
-function navigateToPage() {
-    const select = document.getElementById("userMenu");
-    const selectedValue = select.value;
 
-    if (selectedValue) {
-        window.location.href = selectedValue; // Mengarahkan ke URL yang dipilih
-    }
-}
-=======
-document.addEventListener('DOMContentLoaded', function() {
-    const toggler = document.querySelector('.navbar-toggler');
-    const navbarContent = document.querySelector('#navbarContent');
-
-    toggler.addEventListener('click', function() {
-        navbarContent.classList.toggle('show');
-    });
-});
->>>>>>> c1f39d86056322ebfed3fc0aee7a275b83fbedef
