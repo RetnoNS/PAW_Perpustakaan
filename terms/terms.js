91da2cbadc0cb72be55edbd89aedb9d71f29796d
RetnoNS <119503025+RetnoNS@users.noreply.github.com>
2024-12-14 22:42:32 +0700

    update terms

---
+++ terms/terms.js
@@ -0,0 +1,19 @@
+const agreeCheckbox = document.getElementById("agreeCheckbox");
+const acceptButton = document.getElementById("acceptButton");
+const cancelButton = document.getElementById("cancelButton");
+
+agreeCheckbox.addEventListener("change", () => {
+  if (agreeCheckbox.checked) {
+    acceptButton.disabled = false;
+  } else {
+    acceptButton.disabled = true;
+  }
+});
+
+cancelButton.addEventListener("click", () => {
+  alert("You cancelled the agreement.");
+});
+
+acceptButton.addEventListener("click", () => {
+  alert("You accepted the Terms and Conditions.");
+});
