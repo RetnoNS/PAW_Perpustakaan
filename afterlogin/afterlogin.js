7db06c4ffd27565928b5296e80d1f0d070cad75d
Yusuf Ginanjar <119514056+YusufGinanjar7@users.noreply.github.com>
2024-12-15 15:57:54 +0700

    laravel11
    progres1

--- afterlogin/afterlogin.js
+++
@@ -1,9 +0,0 @@
-function navigateToPage() {
-    const userMenu = document.getElementById("userMenu");
-    const selectedValue = userMenu.value;
-
-    // Pastikan pengguna memilih opsi valid
-    if (selectedValue !== "logout") {
-      window.location.href = selectedValue; // Redirect ke halaman yang dipilih
-    }
-  }
