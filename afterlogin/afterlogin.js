68513f4019b6b15e3b888d8c954d524ead0390d8
nfzmhn <zidninurfauzi2203@gmail.com>
2024-12-24 11:16:02 +0700

    update fix FE

--- afterlogin/afterlogin.js
+++ afterlogin/afterlogin.js
@@ -8,3 +8,10 @@ function navigateToPage() {
     }
   }
 
+  document.querySelectorAll('.book').forEach(book => {
+    book.addEventListener('click', function(event) {
+        if (event.target.tagName === 'BUTTON') return;
+        const url = this.getAttribute('data-url');
+        window.location.href = url;
+    });
+});
